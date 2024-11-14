ddce36a3bf36e0aacc50f8b433098c69a0fb24d2
neab0123 <neab.bkksol@gmail.com>
2024-11-14 09:50:12 +0700

    Change set data func

--- db/line_register.db.js
+++ db/line_register.db.js
@@ -1,10 +1,10 @@
-import { collection, doc } from "firebase/firestore";
+import { collection, doc, setDoc } from "firebase/firestore";
 import db from "./connection";
 
 async function createUser(data){
     try{
         const memberRef = doc(collection(db, "line-noti-register"), "Member") ;
-        const response = await memberRef.set({
+        const response = await setDoc(memberRef, {
             line_id: data.line_id,
             shipped_address: "",
             shipped_name: ""
