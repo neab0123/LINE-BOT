ca37af7015c174242ca48b4d20f52cb3968fe2c5
neab0123 <neab.bkksol@gmail.com>
2024-11-14 10:02:57 +0700

    log to see doc

--- db/line_register.db.js
+++ db/line_register.db.js
@@ -1,14 +1,10 @@
-import { collection, doc, setDoc } from "firebase/firestore";
+import { collection, setDoc, getDoc } from "firebase/firestore";
 import db from "./connection";
 
 async function createUser(data){
     try{
-        const memberRef = doc(collection(db, "line-noti-register"), "Member") ;
-        const response = await setDoc(memberRef, {
-            line_id: data.line_id,
-            shipped_address: "",
-            shipped_name: ""
-        })
+        const memberRef = await getDoc(collection(db, "line-noti-register")) ;
+        console.log(memberRef)
         return response;
     }catch(e){
         console.log("createUser Error: ", e);
