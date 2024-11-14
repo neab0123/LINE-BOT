43a783a3501dd4bf2399a7fa64c559d86e87acb6
neab0123 <neab.bkksol@gmail.com>
2024-11-14 11:16:14 +0700

    test

--- api/lineWebhook.js
+++ api/lineWebhook.js
@@ -42,7 +42,7 @@ export default async function handler(req, res){
             // if(source.type == 'user'){
             //     await sendMessage(source.userId, sendUserId);
             // }
-            console.log(source);
+            console.log(source, events[0]);
             if(userMessage == 'สมัคร'){
                 const message = ""
             }
