30c1ae833eab282ee4bb904c980860b19d294c85
neab0123 <neab.bkksol@gmail.com>
2024-11-21 17:03:50 +0700

    test

--- python-func/cut-message.py
+++ python-func/cut-message.py
@@ -1,4 +1,4 @@
-from pythainlp.tokenize import word_tokenize
+from pythainlp.tokenize import word_tokenize, sent_tokenize
 
 def CutString(string):
     return word_tokenize(string, engine="newmm-safe")
