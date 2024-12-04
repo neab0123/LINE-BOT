34757d50677d88c5e608b3f9824433c33a2162e3
neab0123 <neab.bkksol@gmail.com>
2024-12-04 16:55:12 +0700

    Open register

--- routes/LineWebhook.js
+++ routes/LineWebhook.js
@@ -9,56 +9,48 @@ route.post('/lineWebhook', async (req, res) => {
             const { replyToken, message, source } = events[0];
             const userMessage = message.text;
             const userId = source.userId;
-            const response = await SendLineMessage(userId, userMessage);
 
-            // const findUser = await GetUserByUserId(userId);
-            // if(userMessage == "Register" && findUser == null){
-            //     const replyMessage = "โปรดระบุชื่อ";
-            //     const user = {
-            //         user_id: 0,
-            //         fullname: '',
-            //         address: '',
-            //         mobile: '',
-            //         line_id: userId,
-            //         shipped_status: 0,
-            //         promp_status: 1
-            //     }
+            const findUser = await GetUserByUserId(userId);
+            if(userMessage == "Register" && findUser == null){
+                const replyMessage = "โปรดระบุชื่อ";
+                const user = {
+                    user_id: 0,
+                    fullname: '',
+                    address: '',
+                    mobile: '',
+                    line_id: userId,
+                    shipped_status: 0,
+                    promp_status: 1
+                }
 
-            //     const res = await CreateUser(user);
-            //     await SendLineMessage(userId, replyMessage);
-            //     return;
-            // }
+                const res = await CreateUser(user);
+                await SendLineMessage(userId, replyMessage);
+                return;
+            }
 
-            // if(userMessage == 'Register' && findUser != null){
-            //     const replyMessage = "This line id is already."
-            //     await SendLineMessage(userId, replyMessage);
-            //     return;
-            // }
+            if(findUser != null && findUser.promp_status == 1){
+                const replyMessage = "โปรดระบุเบอร์โทร";
+                findUser.fullname = userMessage;
+                findUser.promp_status = 2;
+                const updateUser = await UpdateUser(userId, findUser);
+                await SendLineMessage(userId, replyMessage);
+            }
 
-            // if(findUser != null && findUser.promp_status == 1){
-            //     const replyMessage = "โปรดระบุเบอร์โทร";
-            //     findUser.fullname = userMessage;
-            //     findUser.promp_status = 2;
-            //     const updateUser = await UpdateUser(userId, findUser);
-            //     await SendLineMessage(userId, replyMessage);
-            // }
+            if(findUser != null && findUser.promp_status == 2){
+                const replyMessage = "โปรดระบุที่อยู่";
+                findUser.mobile = userMessage;
+                findUser.promp_status = 3;
+                const updateUser = await UpdateUser(userId, findUser);
+                await SendLineMessage(userId, replyMessage);
+            }
 
-            // if(findUser != null && findUser.promp_status == 2){
-            //     const replyMessage = "โปรดระบุที่อยู่";
-            //     findUser.mobile = userMessage;
-            //     findUser.promp_status = 3;
-            //     const updateUser = await UpdateUser(userId, findUser);
-            //     await SendLineMessage(userId, replyMessage);
-            // }
-
-            // if(findUser != null && findUser.promp_status == 3){
-            //     const replyMessage = "Thank you";
-            //     findUser.address = userMessage;
-            //     findUser.promp_status = 0;
-            //     const updateUser = await UpdateUser(userId, findUser);
-            //     await SendLineMessage(userId, replyMessage);
-            // }
-            res.status(200).send({ message: userMessage });
+            if(findUser != null && findUser.promp_status == 3){
+                const replyMessage = "Thank you";
+                findUser.address = userMessage;
+                findUser.promp_status = 0;
+                const updateUser = await UpdateUser(userId, findUser);
+                await SendLineMessage(userId, replyMessage);
+            }
         }
         res.status(200).send("OK");
     }catch(error){
