62eceb1ad37af33c4c9bb7cb91f554a3064ba12b
neab0123 <neab.bkksol@gmail.com>
2024-12-04 15:57:26 +0700

    Test

--- api/lineWebhook.js
+++ api/lineWebhook.js
@@ -33,11 +33,11 @@ async function sendMessage(replyToken, message){
 export default async function handler(req, res){
     if(req.method === "POST"){
         const events = req.body.events;
-        // console.log("req:", req.body)
-        // console.log("message:", req.body.events[0].message)
-        // console.log("sources:", req.body.events[0].source)
-        // console.log("source:", req.body.events[0].source)
         if(events && events.length > 0){
+            console.log("req:", req.body)
+            console.log("message:", req.body.events[0].message)
+            console.log("sources:", req.body.events[0].source)
+            console.log("source:", req.body.events[0].source)
             // replytoken for use reply api, userId for use push api
             const { replyToken, message, source } = events[0];
             const userMessage = message.text;
