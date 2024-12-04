6674f6d16f61aefd38c36df409c35fadfe510ee4
neab0123 <neab.bkksol@gmail.com>
2024-12-04 16:24:09 +0700

    delete try catch

--- controllers/LineController.js
+++ controllers/LineController.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 
 async function SendLineMessage(UserId, Message){
-    try{
+    // try{
         const response = await fetch("https://api.line.me/v2/bot/message/push", {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
-                "Authorization": `Bearer 43w0Ib7l6wHZ/RDPJU1pbe8h3qq5Sqr6OS4qbrUlAYcHjvPlFNGaRuf0PIdlzM/2ykWYHKQcdqDApw5xf6/Y/xZqRTWGgtf6Y5T99y7J+2kKnn/BfKTaWWvOFNV1PmIIE0wbiQe88wUkh+9DM4K/ygdB04t89/1O/w1cDnyilFU=`
+                Authorization: `Bearer 43w0Ib7l6wHZ/RDPJU1pbe8h3qq5Sqr6OS4qbrUlAYcHjvPlFNGaRuf0PIdlzM/2ykWYHKQcdqDApw5xf6/Y/xZqRTWGgtf6Y5T99y7J+2kKnn/BfKTaWWvOFNV1PmIIE0wbiQe88wUkh+9DM4K/ygdB04t89/1O/w1cDnyilFU=`
             },
             body: JSON.stringify({
                 to: UserId,
@@ -17,14 +17,12 @@ async function SendLineMessage(UserId, Message){
             })
         })
 
-        console.log(response)
-
         if(!response.ok){
-            console.log(`Failed to send message: ${message}`)
+            throw new Error(`Failed to send message: ${message}`)
         }
-    }catch(error){
-        throw new Error('func sendMessage Error: ', error);
-    }
+    // }catch(error){
+    //     throw new Error('func sendMessage Error: ', error);
+    // }
 }
 
 module.exports = {
