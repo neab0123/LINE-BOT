ba6e830bc7769111e203bb8eb1f00ef1f8c3fe68
Neab012 <neabparinya444@gmail.com>
2024-12-05 20:16:50 +0700

    generate swagger

--- swagger.js
+++ swagger.js
@@ -1,6 +1,6 @@
 const swaggerAutogen = require('swagger-autogen')()
 
 const outputFile = './swagger_output.json'
-const endPoint = ['./routes/*.js']
+const endPoint = ['server.js']
 
 swaggerAutogen(outputFile, endPoint)
