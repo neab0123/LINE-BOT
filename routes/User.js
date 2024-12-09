339733bce9269ab8262a93d6beb540528de94fe9
neab0123 <neab.bkksol@gmail.com>
2024-12-09 09:27:16 +0700

    Add show

--- routes/User.js
+++ routes/User.js
@@ -15,7 +15,7 @@ router.post('/', (req, res) => {
 router.get('/user/:id', async (req, res) => {
     const userId = req.params.id;
     const user = await GetUserByUserId(userId)
-    res.json(userId).status(200);
+    res.json([userId, user]).status(200);
 })
 
 module.exports = router;
