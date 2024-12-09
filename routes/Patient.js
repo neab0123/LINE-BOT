840ff159e21fe1e532b69fd1a902fbbe4561d18d
neab0123 <neab.bkksol@gmail.com>
2024-12-09 09:32:04 +0700

    Test Create test patient

--- routes/Patient.js
+++ routes/Patient.js
@@ -1,10 +1,21 @@
-const { GetPatients } = require('../controllers/PatientController');
+const { GetPatients, CreatePatient } = require('../controllers/PatientController');
 const express = require('express');
 const route = express.Router();
 
 route.get('/', async (req, res) => {
     const list_patient = await GetPatients();
     res.json(list_patient).status(200);
+})
+
+route.post('/', async(req, res) => {
+    const data = {
+        patient_id: 0,
+        fullname: 'test',
+        qr_code: 'test'
+    }
+
+    const resp = await CreatePatient(data);
+    res.json(resp).status(200);
 })
 
 module.exports = route;
